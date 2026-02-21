3725b42ae57c59b3fe34105abe503dd6a8b3df3c
RayZ228 <Almattildahmetov1@gmail.com>
2026-02-21 12:22:07 +0500

    Delete app directory, old version

--- app/crud.py
+++
@@ -1,9 +0,0 @@
-# простое временное хранилище
-grants_db = []
-
-def get_grants():
-    return grants_db
-
-def create_grant(grant):
-    grants_db.append(grant.dict())
-    return grant.dict()
--- app/schemas.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class GrantCreate(BaseModel):
-    title: str
-    organization: str
-    url: str
-    deadline: Optional[str] = None
